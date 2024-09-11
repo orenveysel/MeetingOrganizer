872e8201bbbc7dd0deefe1467b86dc6df98cb4ca
Veysel ÖREN <orenveysel@outlook.com>
2024-09-11 14:42:18 +0300

    added DAL and Entities Layer, added entitiy

---
+++ MeetingOrganizer.Entities/Models/Concrete/Meeting.cs
@@ -0,0 +1,23 @@
+﻿using MeetingOrganizer.Entities.Models.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingOrganizer.Entities.Models.Concrete
+{
+    public class Meeting : BaseEntity
+    {
+        public required string Topic { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan EndTime { get; set; }
+
+        public string? Participants { get; set; }
+    }
+}
---
+++ MeetingOrganizer/Controllers/MeetingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingOrganizer.MVC.Controllers
+{
+    public class MeetingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
