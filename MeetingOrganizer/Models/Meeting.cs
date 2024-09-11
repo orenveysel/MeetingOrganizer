b69ff7d100fc26b52b34717d3b11a3a8736d32d5
Veysel ÖREN <orenveysel@outlook.com>
2024-09-11 14:25:19 +0300

    added meeting view model

---
+++ MeetingOrganizer/Models/Meeting.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetingOrganizer.MVC.Models
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Topic { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public TimeSpan StartTime { get; set; }
+
+        [Required]
+        public TimeSpan EndTime { get; set; }
+
+        public string Participants { get; set; }
+    }
+}
