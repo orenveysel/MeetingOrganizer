b8042a8635ab14beb1c06a10a5ea6ccfa11c0bd6
Veysel ÖREN <orenveysel@outlook.com>
2024-09-11 15:21:00 +0300

    added meeting controller

--- MeetingOrganizer/Controllers/MeetingController.cs
+++ MeetingOrganizer/Controllers/MeetingController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MeetingOrganizer.DAL.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeetingOrganizer.MVC.Controllers
 {
     public class MeetingController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public MeetingController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        
     }
 }
